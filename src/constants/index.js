37768c118893437b2e241493e36c171e7e70fce6
MariaMP2501 <alemendez2501@gmail.com>
2022-08-18 16:47:51 +0200

    Bank app React

---
+++ src/constants/index.js
@@ -0,0 +1,197 @@
+import { people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star  } from "../assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "features",
+    title: "Features",
+  },
+  {
+    id: "product",
+    title: "Product",
+  },
+  {
+    id: "clients",
+    title: "Clients",
+  },
+];
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: star,
+    title: "Bénefits",
+    content:
+      "Les meilleures cartes de crédit offrent des combinaisons alléchantes de promotions et de prix",
+  },
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "100% Sécurisé",
+    content:
+      "Nous prenons des mesures proactives pour nous assurer que vos renseignements et vos transactions sont sécurisés.",
+  },
+  {
+    id: "feature-3",
+    icon: send,
+    title: "Transfer de solde",
+    content:
+      "Une carte de crédit de transfert de solde peut vous faire économiser beaucoup d’argent en frais d’intérêt.",
+  },
+];
+
+export const feedback = [
+  {
+    id: "feedback-1",
+    content:
+      "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
+    name: "Herman Jensen",
+    title: "Founder & Leader",
+    img: people01,
+  },
+  {
+    id: "feedback-2",
+    content:
+      "Money makes your life easier. If you're lucky to have it, you're lucky.",
+    name: "Steve Mark",
+    title: "Founder & Leader",
+    img: people02,
+  },
+  {
+    id: "feedback-3",
+    content:
+      "It is usually people in the money business, finance, and international trade that are really rich.",
+    name: "Kenn Gallagher",
+    title: "Founder & Leader",
+    img: people03,
+  },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "Utilisateurs actifs",
+    value: "3800+",
+  },
+  {
+    id: "stats-2",
+    title: "Nous font confiance",
+    value: "230+",
+  },
+  {
+    id: "stats-3",
+    title: "Transactions",
+    value: "$230M+",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Useful Links",
+    links: [
+      {
+        name: "Contenu",
+        link: "https://www.hoobank.com/content/",
+      },
+      {
+        name: "Comment ça marche",
+        link: "https://www.hoobank.com/how-it-works/",
+      },
+      {
+        name: "Créer",
+        link: "https://www.hoobank.com/create/",
+      },
+      {
+        name: "Explorer",
+        link: "https://www.hoobank.com/explore/",
+      },
+      {
+        name: "Termes & Services",
+        link: "https://www.hoobank.com/terms-and-services/",
+      },
+    ],
+  },
+  {
+    title: "Communauté",
+    links: [
+      {
+        name: "Centre d'aide",
+        link: "https://www.hoobank.com/help-center/",
+      },
+      {
+        name: "Partnaires",
+        link: "https://www.hoobank.com/partners/",
+      },
+      {
+        name: "Suggestions",
+        link: "https://www.hoobank.com/suggestions/",
+      },
+      {
+        name: "Blog",
+        link: "https://www.hoobank.com/blog/",
+      },
+      {
+        name: "Newsletters",
+        link: "https://www.hoobank.com/newsletters/",
+      },
+    ],
+  },
+  {
+    title: "Partner",
+    links: [
+      {
+        name: "Nos Partenaires",
+        link: "https://www.hoobank.com/our-partner/",
+      },
+      {
+        name: "Devenir partenaire",
+        link: "https://www.hoobank.com/become-a-partner/",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: twitter,
+    link: "https://www.twitter.com/",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://www.linkedin.com/",
+  },
+];
+
+export const clients = [
+  {
+    id: "client-1",
+    logo: airbnb,
+  },
+  {
+    id: "client-2",
+    logo: binance,
+  },
+  {
+    id: "client-3",
+    logo: coinbase,
+  },
+  {
+    id: "client-4",
+    logo: dropbox,
+  },
+];
